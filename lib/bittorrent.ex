50a51ef7e3d215280bc96ccb0f9a2f7933728597
ProgMastermind <surya.21mis7126@vitapstudent.ac.in>
2024-09-02 12:03:38 +0530

    codecrafters submit [skip ci]

--- lib/bittorrent.ex
+++ lib/bittorrent.ex
@@ -15,16 +15,40 @@ defmodule Bittorrent.CLI do
 end
 
 defmodule Bencode do
-    def decode(encoded_value) when is_binary(encoded_value) do
-        binary_data = :binary.bin_to_list(encoded_value)
-        case Enum.find_index(binary_data, fn char -> char == 58 end) do
-          nil ->
-            IO.puts("The ':' character is not found in the binary")
-          index ->
-            rest = Enum.slice(binary_data, index+1..-1)
-            List.to_string(rest)
-        end
-      end
+  def decode(encoded_value) when is_binary(encoded_value) do
+    binary_data = :binary.bin_to_list(encoded_value)
 
-    def decode(_), do: "Invalid encoded value: not binary"
+    case find_colon_index(binary_data) do
+      nil ->
+        case find_integer_start(binary_data) do
+          nil ->
+            IO.puts("Neither ':' nor 'i' character found in the binary")
+            nil
+          integer_start_index ->
+            decode_integer(binary_data, integer_start_index)
+        end
+      colon_index ->
+        decode_string(binary_data, colon_index)
+    end
+  end
+
+  def decode(_), do: "Invalid encoded value: not binary"
+
+  defp find_colon_index(binary_data) do
+    Enum.find_index(binary_data, fn char -> char == 58 end)
+  end
+
+  defp find_integer_start(binary_data) do
+    Enum.find_index(binary_data, fn char -> char == 105 end)
+  end
+
+  defp decode_string(binary_data, index) do
+    rest = Enum.slice(binary_data, index + 1..-1)
+    List.to_string(rest)
+  end
+
+  defp decode_integer(binary_data, index) do
+    rest = Enum.slice(binary_data, index+1..-2)
+    List.to_integer(rest)
+  end
 end
